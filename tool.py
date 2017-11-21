631a97de0443473a1fea5c80b2b4b760869c8fa8
edbro <oskar@edbro.se>
2017-11-21 10:50:18 +0100

    Fixed argument parsing

--- tool.py
+++ tool.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 
+import sys
+
+def main(pubpath, privpath):
+    
 
 
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        main(sys.argv[1], sys.argv[2])
+    else:
+        print("Usage tool.py pubpath privpath\n")
+
+        print("where pubpath is the path to a folder containing \npublic keys\
+ and privpath is the path to a folder \ncontaining private keys.")
