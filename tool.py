a5a2c7b57de4b7ba29309447876f654aa68ec620
Blacka <annika.hansson@saffran.se>
2017-11-28 12:00:08 +0100

    All keys in the dirs are looped through

--- tool.py
+++ tool.py
@@ -8,46 +8,51 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 #from OpenSSL import crypto
 
-debug = True
+debug = False
 
 def main(pubpath, privpath):
     publist = os.listdir(pubpath)
     privlist = os.listdir(privpath)
     if debug:
         print("content of publist " + str(publist))
-    
-    with open(pubpath + '/' + publist[0], 'rb') as pubfile:
-        pubdata = pubfile.read()
+        print("content of privlist " + str(privlist))
+ 
+    for i in range(0,len(publist)): 
+        with open(pubpath + '/' + publist[i], 'rb') as pubfile:
+            pubdata = pubfile.read()
+            if debug:
+                print("pubdata " + str(pubdata))
+        # pubkey = crypto.load_publickey(crypto.FILETYPE_PEM, pubdata) 
+        pubkey = serialization.load_ssh_public_key(pubdata,\
+                backends.default_backend()) 
         if debug:
-            print("pubdata " + str(pubdata))
-    # pubkey = crypto.load_publickey(crypto.FILETYPE_PEM, pubdata)
-    pubkey = serialization.load_ssh_public_key(pubdata,\
-            backends.default_backend()) 
-    if debug:
-        print("pubkey: " + str(pubkey))
+            print("pubkey: " + str(pubkey))
+ 
+        for j in range(0,len(privlist)): 
+            with open(privpath + '/' + privlist[0], 'rb') as privfile:
+                privdata = privfile.read()
+                if debug:
+                    print("privdata " + str(privdata))
+            # pubkey = crypto.load_publickey(crypto.FILETYPE_PEM, pubdata)
+            privkey = serialization.load_pem_private_key(privdata,\
+                    None, backends.default_backend()) 
+            if debug:
+                print("privkey: " + str(privkey))
     
-    with open(privpath + '/' + privlist[0], 'rb') as privfile:
-        privdata = privfile.read()
-        if debug:
-            print("privdata " + str(privdata))
-    # pubkey = crypto.load_publickey(crypto.FILETYPE_PEM, pubdata)
-    privkey = serialization.load_pem_private_key(privdata,\
-            None, backends.default_backend()) 
-    if debug:
-        print("privkey: " + str(privkey))
-    
-    #testkey = privkey.public_key()
-    #if debug:
-    #    print("testkey: " + str(testkey))
+            #testkey = privkey.public_key()
+            #if debug:
+            #    print("testkey: " + str(testkey))
    
-    pad= padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA1()),\
-            algorithm=hashes.SHA1(),\
-            label=None)
-    msg = pubkey.encrypt(b"True", pad)
+            pad= padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA1()),\
+                    algorithm=hashes.SHA1(),\
+                    label=None)
+            msg = pubkey.encrypt(b"True", pad) 
     
-    
-    if privkey.decrypt(msg, pad) == b"True":
-        print("YAY")
+            try:
+                if privkey.decrypt(msg, pad) == b"True":
+                    print("YAY")
+            except ValueError:
+                print("Oh no...")
 
 if __name__ == "__main__":
     if len(sys.argv) == 3:
