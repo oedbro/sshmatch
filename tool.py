a64eaf52f6281f053d8f1e78d9a0efb4e7a2c769
edbro <oskar@edbro.se>
2017-11-28 15:36:47 +0100

    Refactoring and added some comments

--- tool.py
+++ tool.py
@@ -6,53 +6,49 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat import backends
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
-#from OpenSSL import crypto
 
+# set to true for debug output
 debug = False
 
 def main(pubpath, privpath):
+    # generate a list of the keys to test
     publist = os.listdir(pubpath)
     privlist = os.listdir(privpath)
     if debug:
         print("content of publist " + str(publist))
         print("content of privlist " + str(privlist))
  
-    for i in range(0,len(publist)): 
-        with open(pubpath + '/' + publist[i], 'rb') as pubfile:
+    for pub in publist: 
+        with open(pubpath + '/' + pub, 'rb') as pubfile:
             pubdata = pubfile.read()
             if debug:
                 print("pubdata " + str(pubdata))
-        # pubkey = crypto.load_publickey(crypto.FILETYPE_PEM, pubdata) 
         pubkey = serialization.load_ssh_public_key(pubdata,\
                 backends.default_backend()) 
         if debug:
-            print("pubkey: " + str(pubkey))
+            print("pubkey: " + str(pub))
 
 
         pad= padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA1()),\
                 algorithm=hashes.SHA1(),\
                 label=None)
         
-        for j in range(0,len(privlist)): 
-            with open(privpath + '/' + privlist[j], 'rb') as privfile:
+        for priv in privlist: 
+            with open(privpath + '/' + priv, 'rb') as privfile:
                 privdata = privfile.read()
                 if debug:
                     print("privdata " + str(privdata))
-            # pubkey = crypto.load_publickey(crypto.FILETYPE_PEM, pubdata)
             privkey = serialization.load_pem_private_key(privdata,\
                     None, backends.default_backend()) 
             if debug:
                 print("privkey: " + str(privkey))
     
-            #testkey = privkey.public_key()
-            #if debug:
-            #    print("testkey: " + str(testkey))
-   
             msg = pubkey.encrypt(b"True", pad) 
     
             try:
                 if privkey.decrypt(msg, pad) == b"True":
-                    print("Match: " + publist[i] + " " + privlist[j])
+                    print("Match: " + pubpath + "/" + pub + " " + privpath +\
+                    "/" + priv)
             except ValueError:
                 pass
 
